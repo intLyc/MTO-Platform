143bd145ac27b7527466871df98066da5daaa635
int-lyc <int_lyc@outlook.com>
2021-10-21 23:35:46 +0800

    change mtsoco problems name

--- Problems/MTSOCO/MTSO9_NI_LS.m
+++ Problems/MTSOCO/MTSO9_NI_LS.m
@@ -1,26 +1,26 @@
-classdef PI_H < Problem
+classdef MTSO9_NI_LS < Problem
 
     properties
     end
 
     methods
 
-        function obj = PI_H(name)
+        function obj = MTSO9_NI_LS(name)
             obj = obj@Problem(name);
             obj.dims = [50, 50];
-            obj.tasks_name = {'Rastrigin', 'Sphere'};
+            obj.tasks_name = {'Rastrigin', 'Schwefel'};
         end
 
         function Tasks = getTasks(obj)
-            load('PI_H.mat') % loading data from folder ./Tasks
+            load('NI_L.mat') % loading data from folder ./Tasks
             Tasks(1).dims = obj.dims(1);
             Tasks(1).fnc = @(x)Rastrigin(x, Rotation_Task1, GO_Task1);
             Tasks(1).Lb = -50 * ones(1, obj.dims(1));
             Tasks(1).Ub = 50 * ones(1, obj.dims(1));
             Tasks(2).dims = obj.dims(2);
-            Tasks(2).fnc = @(x)Sphere(x, GO_Task2);
-            Tasks(2).Lb = -100 * ones(1, obj.dims(2));
-            Tasks(2).Ub = 100 * ones(1, obj.dims(2));
+            Tasks(2).fnc = @(x)Schwefel(x);
+            Tasks(2).Lb = -500 * ones(1, obj.dims(2));
+            Tasks(2).Ub = 500 * ones(1, obj.dims(2));
         end
 
     end
