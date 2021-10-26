7ed1a834cdac08ada83dde4dd3f9841692a51e3a
int-lyc <int_lyc@outlook.com>
2021-10-26 16:48:03 +0800

    update

--- Problems/Problem.m
+++ Problems/Problem.m
@@ -2,16 +2,13 @@ classdef Problem < handle
 
     properties
         name % problem's name
-        dims % tasks dim list
-        tasks_name % tasks name cell
     end
 
     methods
 
         function obj = Problem(name)
             % problem constructor
-            % should be inherited as obj@Problem(name)
-            % and set obj.dims, obj.tasks_name
+            % cannot be changed
 
             obj.name = name;
         end
@@ -30,45 +27,15 @@ classdef Problem < handle
             obj.name = name;
         end
 
-        function num = getTasksNumber(obj)
-            % get problem's tasks number
-            % cannot be changed
-
-            num = length(obj.dims);
-        end
-
-        function tasks_name = getTasksName(obj)
-            % get problem's tasks name
-            % cannot be changed
-
-            tasks_name = obj.tasks_name;
-        end
-
-        function parameter = getParameter(obj)
-            % get problem's parameter {task1 name, task1 dim, ...}
-            % cannot be changed
-
-            parameter = {};
-
-            for i = 1:length(obj.tasks_name)
-                parameter = [parameter, [obj.tasks_name{i}, ' dim'], num2str(obj.dims(i))];
-            end
-
-        end
-
-        function obj = setParameter(obj, parameter_cell)
-            % set problem's parameter {task1 dim, ...}
-            % cannot be changed
-
-            for i = 1:length(obj.dims)
-                obj.dims(i) = str2num(parameter_cell{i});
-            end
-
-        end
-
     end
 
     methods (Abstract)
+
+        getParameter(obj) % get problem's parameter
+        % return parameter, contains {para1, value1, para2, value2, ...} (string)
+
+        setParameter(obj, parameter_cell) % set problem's parameter
+        % arg parameter_cell, contains {value1, value2, ...} (string)
 
         getTasks(obj) % get problem's tasks
         % return tasks, contains [task1, task2, ...]
