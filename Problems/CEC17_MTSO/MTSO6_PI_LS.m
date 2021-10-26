84b2ce7cb0bfc1128899fd33f5a0f653a9ba75cd
int-lyc <int_lyc@outlook.com>
2021-10-26 16:47:52 +0800

    update cec17 mtso

---
+++ Problems/CEC17_MTSO/MTSO6_PI_LS.m
@@ -0,0 +1,29 @@
+classdef MTSO6_PI_LS < Problem
+
+    properties
+    end
+
+    methods
+        function parameter = getParameter(obj)
+            parameter = {};
+        end
+
+        function obj = setParameter(obj, parameter_cell)
+        end
+
+        function Tasks = getTasks(obj)
+            dims = [50 25];
+            load('PI_L.mat') % loading data from folder ./Tasks
+            Tasks(1).dims = dims(1);
+            Tasks(1).fnc = @(x)Ackley(x, Rotation_Task1, GO_Task1);
+            Tasks(1).Lb = -50 * ones(1, dims(1));
+            Tasks(1).Ub = 50 * ones(1, dims(1));
+            Tasks(2).dims = dims(2);
+            Tasks(2).fnc = @(x)Weierstrass(x, Rotation_Task2, GO_Task2);
+            Tasks(2).Lb = -0.5 * ones(1, dims(2));
+            Tasks(2).Ub = 0.5 * ones(1, dims(2));
+        end
+
+    end
+
+end
