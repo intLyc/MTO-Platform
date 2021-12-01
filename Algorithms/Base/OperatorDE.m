8ef5cb368e20128a5a17eac8398cf4d12507cb13
intLyc <int_lyc@outlook.com>
2021-12-01 21:48:37 +0800

    update

--- Algorithms/Base/OperatorDE.m
+++ Algorithms/Base/OperatorDE.m
@@ -23,6 +23,53 @@ classdef OperatorDE
             end
         end
 
+        function [offspring, calls] = generateMF(callfun, population, Tasks, rmp, F, pCR)
+            count = 1;
+            for i = 1:ceil(length(population) / 2)
+                p1 = indorder(i);
+                p2 = indorder(i + fix(length(population) / 2));
+                offspring(count) = Individual();
+                offspring(count).factorial_costs = inf(1, length(Tasks));
+                offspring(count + 1) = Individual();
+                offspring(count + 1).factorial_costs = inf(1, length(Tasks));
+
+                if (population(p1).skill_factor == population(p2).skill_factor) || rand < rmp
+                    u = rand(1, max([Tasks.dims]));
+                    cf = zeros(1, max([Tasks.dims]));
+                    cf(u <= 0.5) = (2 * u(u <= 0.5)).^(1 / (mu + 1));
+                    cf(u > 0.5) = (2 * (1 - u(u > 0.5))).^(-1 / (mu + 1));
+
+                    offspring(count) = OperatorGA.crossover(offspring(count), population(p1), population(p2), cf);
+                    offspring(count + 1) = OperatorGA.crossover(offspring(count + 1), population(p2), population(p1), cf);
+
+                    p = [p1, p2];
+                    offspring(count).skill_factor = population(p(randi(2))).skill_factor;
+                    offspring(count + 1).skill_factor = population(p(randi(2))).skill_factor;
+                else
+                    offspring(count) = OperatorGA.mutate(population(p1), max([Tasks.dims]), mum);
+                    offspring(count + 1) = OperatorGA.mutate(population(p2), max([Tasks.dims]), mum);
+                end
+                for x = count:count + 1
+                    offspring(x).rnvec(offspring(x).rnvec > 1) = 1;
+                    offspring(x).rnvec(offspring(x).rnvec < 0) = 0;
+                end
+                count = count + 2;
+            end
+            if callfun
+                offspring_temp = Individual.empty();
+                calls = 0;
+                for t = 1:length(Tasks)
+                    offspring_t = offspring([offspring.skill_factor] == t);
+                    [offspring_t, cal] = evaluate(offspring_t, Tasks(t), t);
+                    offspring_temp = [offspring_temp, offspring_t];
+                    calls = calls + cal;
+                end
+                offspring = offspring_temp;
+            else
+                calls = 0;
+            end
+        end
+
         function object = mutate_rand_1(object, x1, x2, x3, F)
             object.rnvec = x1.rnvec + F * (x2.rnvec - x3.rnvec);
         end
--- Algorithms/Base/evaluate.m
+++ Algorithms/Base/evaluate.m
@@ -1,7 +1,7 @@
 function [population, calls] = evaluate(population, Task, task_idx)
     for i = 1:length(population)
-        x = (Task.Ub - Task.Lb) .* population(i).rnvec + Task.Lb;
-        population(i).factorial_costs(task_idx) = Task.fnc(x(1:Task.dims));
+        x = (Task.Ub - Task.Lb) .* population(i).rnvec(1:Task.dims) + Task.Lb;
+        population(i).factorial_costs(task_idx) = Task.fnc(x);
     end
     calls = length(population);
 end
