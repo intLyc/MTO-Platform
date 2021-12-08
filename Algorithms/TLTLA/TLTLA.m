72f92a5979f7264111d73ddccef33ee53b760880
int-lyc <int_lyc@outlook.com>
2021-12-08 16:39:15 +0800

    change TLTLA generation logic

--- Algorithms/TLTLA/TLTLA.m
+++ Algorithms/TLTLA/TLTLA.m
@@ -45,66 +45,65 @@ classdef TLTLA < Algorithm
             while generation < iter_num && fnceval_calls < eva_num
                 generation = generation + 1;
 
-                if mod(generation, 2) == 0
-                    % Intra-Task Knowledge Transfer
-                    parent = randi(length(population));
-                    t = population(parent).skill_factor;
-                    dimen = mod(generation - 2, max([Tasks.dims])) + 1; % start with 1 Dim
-                    child_rnvec = zeros(size(population(parent)));
-                    pool = population([population.skill_factor] == t);
-                    for d = 1:max([Tasks.dims])
-                        x = randperm(length(pool), min(3, length(pool)));
-                        if length(pool) < 3
-                            child_rnvec(d) = pool(x(1)).rnvec(dimen);
-                            continue;
-                        end
-                        if rand > 0.5
-                            child_rnvec(d) = pool(x(1)).rnvec(dimen) + 0.5 * rand * (pool(x(2)).rnvec(dimen) - pool(x(3)).rnvec(dimen));
-                        else
-                            child_rnvec(d) = pool(x(1)).rnvec(dimen) + 0.5 * rand * (pool(x(3)).rnvec(dimen) - pool(x(2)).rnvec(dimen));
-                        end
-                    end
-                    child_rnvec(child_rnvec > 1) = 1;
-                    child_rnvec(child_rnvec < 0) = 0;
+                % Inter-Task Knowledge Transfer
+                % generation
+                [offspring, calls] = OperatorGA.generateMF(1, population, Tasks, obj.rmp, obj.mu, obj.mum);
+                fnceval_calls = fnceval_calls + calls;
+                % selection
+                [population, bestobj, data.bestX] = selectMF(population, offspring, Tasks, pop_size, bestobj, data.bestX);
 
+                % Intra-Task Knowledge Transfer
+                parent = randi(length(population));
+                t = population(parent).skill_factor;
+                dimen = mod(generation - 2, max([Tasks.dims])) + 1; % start with 1 Dim
+                child_rnvec = zeros(size(population(parent)));
+                pool = population([population.skill_factor] == t);
+                for d = 1:max([Tasks.dims])
+                    x = randperm(length(pool), min(3, length(pool)));
+                    if length(pool) < 3
+                        child_rnvec(d) = pool(x(1)).rnvec(dimen);
+                        continue;
+                    end
                     if rand > 0.5
-                        tmp_population = population(parent);
-                        for d = 1:max([Tasks.dims])
-                            tmp_population.rnvec(d) = child_rnvec(d);
-                            [tmp_population, calls] = evaluate(tmp_population, Tasks(t), t);
-                            fnceval_calls = fnceval_calls + calls;
-
-                            if tmp_population.factorial_costs(t) < population(parent).factorial_costs(t)
-                                population(parent) = tmp_population;
-                                break;
-                            end
-                        end
+                        child_rnvec(d) = pool(x(1)).rnvec(dimen) + 0.5 * rand * (pool(x(2)).rnvec(dimen) - pool(x(3)).rnvec(dimen));
                     else
-                        for d = 1:max([Tasks.dims])
-                            tmp_population = population(parent);
-                            tmp_population.rnvec(d) = child_rnvec(d);
-                            [tmp_population, calls] = evaluate(tmp_population, Tasks(t), t);
-                            fnceval_calls = fnceval_calls + calls;
-
-                            if tmp_population.factorial_costs(t) < population(parent).factorial_costs(t)
-                                population(parent) = tmp_population;
-                                break;
-                            end
-                        end
+                        child_rnvec(d) = pool(x(1)).rnvec(dimen) + 0.5 * rand * (pool(x(3)).rnvec(dimen) - pool(x(2)).rnvec(dimen));
                     end
+                end
+                child_rnvec(child_rnvec > 1) = 1;
+                child_rnvec(child_rnvec < 0) = 0;
 
-                    if population(parent).factorial_costs(t) < bestobj(t)
-                        bestobj(t) = population(parent).factorial_costs(t);
-                        bestX{t} = population(parent).rnvec;
+                if rand > 0.5
+                    tmp_population = population(parent);
+                    for d = 1:max([Tasks.dims])
+                        tmp_population.rnvec(d) = child_rnvec(d);
+                        [tmp_population, calls] = evaluate(tmp_population, Tasks(t), t);
+                        fnceval_calls = fnceval_calls + calls;
+
+                        if tmp_population.factorial_costs(t) < population(parent).factorial_costs(t)
+                            population(parent) = tmp_population;
+                            break;
+                        end
                     end
                 else
-                    % Inter-Task Knowledge Transfer
-                    % generation
-                    [offspring, calls] = OperatorGA.generateMF(1, population, Tasks, obj.rmp, obj.mu, obj.mum);
-                    fnceval_calls = fnceval_calls + calls;
-                    % selection
-                    [population, bestobj, data.bestX] = selectMF(population, offspring, Tasks, pop_size, bestobj, data.bestX);
+                    for d = 1:max([Tasks.dims])
+                        tmp_population = population(parent);
+                        tmp_population.rnvec(d) = child_rnvec(d);
+                        [tmp_population, calls] = evaluate(tmp_population, Tasks(t), t);
+                        fnceval_calls = fnceval_calls + calls;
+
+                        if tmp_population.factorial_costs(t) < population(parent).factorial_costs(t)
+                            population(parent) = tmp_population;
+                            break;
+                        end
+                    end
                 end
+
+                if population(parent).factorial_costs(t) < bestobj(t)
+                    bestobj(t) = population(parent).factorial_costs(t);
+                    bestX{t} = population(parent).rnvec;
+                end
+
                 data.convergence(:, generation) = bestobj;
             end
             data.bestX = bin2real(data.bestX, Tasks);
