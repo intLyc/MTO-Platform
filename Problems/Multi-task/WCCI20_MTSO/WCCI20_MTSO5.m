b5eefce5bacea872823b40925d80ba3fbc829835
int-lyc <int_lyc@outlook.com>
2022-02-01 22:01:40 +0800

    reset path

---
+++ Problems/Multi-task/WCCI20_MTSO/WCCI20_MTSO5.m
@@ -0,0 +1,21 @@
+classdef WCCI20_MTSO5 < Problem
+
+    properties
+    end
+
+    methods
+        function parameter = getParameter(obj)
+            parameter = obj.getRunParameter();
+        end
+
+        function obj = setParameter(obj, parameter_cell)
+            obj.setRunParameter(parameter_cell(1:3));
+        end
+
+        function Tasks = getTasks(obj)
+            Tasks = benchmark_WCCI20_MTSO(5);
+        end
+
+    end
+
+end
---
+++ Problems/Multi-task/WCCI20_MTSO/WCCI20_MTSO9.m
@@ -0,0 +1,21 @@
+classdef WCCI20_MTSO9 < Problem
+
+    properties
+    end
+
+    methods
+        function parameter = getParameter(obj)
+            parameter = obj.getRunParameter();
+        end
+
+        function obj = setParameter(obj, parameter_cell)
+            obj.setRunParameter(parameter_cell(1:3));
+        end
+
+        function Tasks = getTasks(obj)
+            Tasks = benchmark_WCCI20_MTSO(9);
+        end
+
+    end
+
+end
