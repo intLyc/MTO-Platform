32903f2575acbf5002e4fa1ccca0ad2ca9364fad
intLyc <int_lyc@outlook.com>
2022-04-17 17:07:30 +0800

    change folder

---
+++ Algorithms/Multi-task/MFDE/OperatorMFDE.m
@@ -0,0 +1,62 @@
+classdef OperatorMFDE < OperatorDE
+    methods (Static)
+        function [offspring, calls] = generate(callfun, population, Tasks, rmp, F, CR)
+            if isempty(population)
+                offspring = population;
+                calls = 0;
+                return;
+            end
+            Individual_class = class(population(1));
+            group = cell([1, length(Tasks)]);
+            for i = 1:length(population)
+                group{population(i).skill_factor} = [group{population(i).skill_factor}, i];
+            end
+            for i = 1:length(population)
+                offspring(i) = feval(Individual_class);
+                offspring(i).factorial_costs = inf(1, length(Tasks));
+                offspring(i).constraint_violation = inf(1, length(Tasks));
+
+                other = [];
+                for t = 1:length(group)
+                    if population(i).skill_factor ~= t
+                        other = [other, group{t}];
+                    end
+                end
+                other = other(randperm(length(other)));
+
+                A = randperm(length(group{population(i).skill_factor}));
+                A = group{population(i).skill_factor}(A);
+                A(A == i) = [];
+                x1 = A(1);
+                if rand < rmp
+                    x2 = other(mod(2 - 1, length(other)) + 1);
+                    x3 = other(mod(3 - 1, length(other)) + 1);
+                    offspring(i).skill_factor = population(x2).skill_factor;
+                else
+                    x2 = A(mod(2 - 1, length(A)) + 1);
+                    x3 = A(mod(3 - 1, length(A)) + 1);
+                    offspring(i).skill_factor = population(x1).skill_factor;
+                end
+
+                offspring(i) = OperatorDE.mutate_rand_1(offspring(i), population(x1), population(x2), population(x3), F);
+                offspring(i) = OperatorDE.crossover(offspring(i), population(i), CR);
+
+                offspring(i).rnvec(offspring(i).rnvec > 1) = 1;
+                offspring(i).rnvec(offspring(i).rnvec < 0) = 0;
+            end
+            if callfun
+                offspring_temp = feval(Individual_class).empty();
+                calls = 0;
+                for t = 1:length(Tasks)
+                    offspring_t = offspring([offspring.skill_factor] == t);
+                    [offspring_t, cal] = evaluate(offspring_t, Tasks(t), t);
+                    offspring_temp = [offspring_temp, offspring_t];
+                    calls = calls + cal;
+                end
+                offspring = offspring_temp;
+            else
+                calls = 0;
+            end
+        end
+    end
+end
---
+++ Algorithms/Multi-task/MFPSO/OperatorMFPSO.m
@@ -0,0 +1,50 @@
+classdef OperatorMFPSO < Operator
+    methods (Static)
+        function [population, calls] = generate(callfun, population, Tasks, rmp, w, c1, c2, c3, gbest)
+            if isempty(population)
+                calls = 0;
+                return;
+            end
+            Individual_class = class(population(1));
+            for i = 1:length(population)
+                population(i) = OperatorMFPSO.velocityUpdateMF(population(i), gbest, rmp, w, c1, c2, c3, length(Tasks));
+                population(i) = OperatorPSO.positionUpdate(population(i));
+                population(i) = OperatorPSO.pbestUpdate(population(i));
+            end
+            if callfun
+                population_temp = feval(Individual_class).empty();
+                calls = 0;
+                for t = 1:length(Tasks)
+                    population_t = population([population.skill_factor] == t);
+                    [population_t, cal] = evaluate(population_t, Tasks(t), t);
+                    population_temp = [population_temp, population_t];
+                    calls = calls + cal;
+                end
+                population = population_temp;
+            else
+                calls = 0;
+            end
+        end
+
+        function object = velocityUpdateMF(object, gbest, rmp, w, c1, c2, c3, tasks_num)
+            % Multifactorial - Velocity update
+            len = length(object.velocity);
+            if rand < rmp
+                % random select other task
+                other_task = randi(tasks_num);
+                while other_task == object.skill_factor
+                    other_task = randi(tasks_num);
+                end
+
+                object.velocity = w * object.velocity + ...
+                    c1 * rand(1, len) .* (object.pbest - object.rnvec) + ...
+                    c2 * rand(1, len) .* (gbest{object.skill_factor} - object.rnvec) + ...
+                    c3 * rand(1, len) .* (gbest{other_task} - object.rnvec);
+            else
+                object.velocity = w * object.velocity + ...
+                    c1 * rand(1, len) .* (object.pbest - object.rnvec) + ...
+                    c2 * rand(1, len) .* (gbest{object.skill_factor} - object.rnvec);
+            end
+        end
+    end
+end
