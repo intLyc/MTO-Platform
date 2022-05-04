e5c71490e3d287c0f5ca63d9054647bfb00dda1e
intLyc <int_lyc@outlook.com>
2022-05-04 15:38:57 +0800

    modified

--- Algorithms/Multi-task/MFDE/OperatorMFDE.m
+++ Algorithms/Multi-task/MFDE/OperatorMFDE.m
@@ -15,35 +15,35 @@ classdef OperatorMFDE < OperatorDE
                 return;
             end
             Individual_class = class(population(1));
-            group = cell([1, length(Tasks)]);
-            for i = 1:length(population)
-                group{population(i).skill_factor} = [group{population(i).skill_factor}, i];
-            end
             for i = 1:length(population)
                 offspring(i) = feval(Individual_class);
                 offspring(i).factorial_costs = inf(1, length(Tasks));
                 offspring(i).constraint_violation = inf(1, length(Tasks));
 
-                other = [];
-                for t = 1:length(group)
-                    if population(i).skill_factor ~= t
-                        other = [other, group{t}];
-                    end
+                x1 = randi(length(population));
+                while x1 == i || population(x1).skill_factor ~= population(i).skill_factor
+                    x1 = randi(length(population));
                 end
-                other = other(randperm(length(other)));
-
-                A = randperm(length(group{population(i).skill_factor}));
-                A = group{population(i).skill_factor}(A);
-                A(A == i) = [];
-                x1 = A(1);
                 if rand < rmp
-                    x2 = other(mod(2 - 1, length(other)) + 1);
-                    x3 = other(mod(3 - 1, length(other)) + 1);
+                    x2 = randi(length(population));
+                    while population(x2).skill_factor == population(i).skill_factor
+                        x2 = randi(length(population));
+                    end
+                    x3 = randi(length(population));
+                    while x3 == x2 || population(x3).skill_factor == population(i).skill_factor
+                        x3 = randi(length(population));
+                    end
                     offspring(i).skill_factor = population(x2).skill_factor;
                 else
-                    x2 = A(mod(2 - 1, length(A)) + 1);
-                    x3 = A(mod(3 - 1, length(A)) + 1);
-                    offspring(i).skill_factor = population(x1).skill_factor;
+                    x2 = randi(length(population));
+                    while x2 == i || x2 == x1 || population(x2).skill_factor ~= population(i).skill_factor
+                        x2 = randi(length(population));
+                    end
+                    x3 = randi(length(population));
+                    while x3 == i || x3 == x1 || x3 == x2 || population(x3).skill_factor ~= population(i).skill_factor
+                        x3 = randi(length(population));
+                    end
+                    offspring(i).skill_factor = population(i).skill_factor;
                 end
 
                 offspring(i) = OperatorDE.mutate(offspring(i), population(x1), population(x2), population(x3), F);
