dcde5b37f4bde10670ccb8246e5225f5633c0b02
intLyc <int_lyc@outlook.com>
2022-05-04 13:38:46 +0800

    modified

--- Algorithms/Algorithm.m
+++ Algorithms/Algorithm.m
@@ -28,15 +28,20 @@ classdef Algorithm < handle
             % Set algorithm's name, cannot be changed
             obj.name = name;
         end
+
+        function parameter = getParameter(obj)
+            % Get algorithm's parameter
+            % return parameter, contains {para1, value1, para2, value2, ...} (string)
+            parameter = {};
+        end
+
+        function obj = setParameter(obj, parameter_cell)
+            % set algorithm's parameter
+            % arg parameter_cell, contains {value1, value2, ...} (string)
+        end
     end
 
     methods (Abstract)
-        getParameter(obj) % get algorithm's parameter
-        % return parameter, contains {para1, value1, para2, value2, ...} (string)
-
-        setParameter(obj, parameter_cell) % set algorithm's parameter
-        % arg parameter_cell, contains {value1, value2, ...} (string)
-
         run(obj, Tasks) % run this tasks with algorithm,
         % return data, contains data.clock_time, data.convergence
     end
