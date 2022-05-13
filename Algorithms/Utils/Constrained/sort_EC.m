3134eefa455bd489b874ad8c785c36e2a80eae40
intLyc <int_lyc@outlook.com>
2022-05-13 15:25:49 +0800

    fix bug

--- Algorithms/Utils/Constrained/sort_EC.m
+++ Algorithms/Utils/Constrained/sort_EC.m
@@ -1,6 +1,6 @@
 function rank = sort_EC(obj, cv, ep)
     %% Epsilon Constraint Sort
     cv(cv < ep) = 0;
-    [~, rank] = sortrows([cv'; obj']);
+    [~, rank] = sortrows([cv', obj'], [1, 2]);
     rank = rank';
 end
--- Algorithms/Utils/Constrained/sort_FP.m
+++ Algorithms/Utils/Constrained/sort_FP.m
@@ -1,5 +1,5 @@
 function rank = sort_FP(obj, cv)
     %% Feasible Priority Sort
-    [~, rank] = sortrows([cv'; obj']);
+    [~, rank] = sortrows([cv', obj'], [1, 2]);
     rank = rank';
 end
