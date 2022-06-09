8472a20ee69a6514b49298bd635a35edba991918
intLyc <int_lyc@outlook.com>
2022-06-09 17:16:37 +0800

    add CEC10-CSO

---
+++ Problems/Single-task/CEC10_CSO/Class/CEC10_CSO16.m
@@ -0,0 +1,31 @@
+classdef CEC10_CSO16 < Problem
+    % <Single> <Constrained>
+
+    %------------------------------- Copyright --------------------------------
+    % Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
+    % research purposes. All publications which use this platform or any code
+    % in the platform should acknowledge the use of "MTO-Platform" and cite
+    % or footnote "https://github.com/intLyc/MTO-Platform"
+    %--------------------------------------------------------------------------
+
+    methods
+        function obj = CEC10_CSO16(name)
+            obj = obj@Problem(name);
+            obj.sub_eva = 20000 * obj.dims;
+        end
+
+        function parameter = getParameter(obj)
+            parameter = {'Dims', num2str(obj.dims)};
+            parameter = [obj.getRunParameter(), parameter];
+        end
+
+        function obj = setParameter(obj, parameter_cell)
+            obj.setRunParameter(parameter_cell(1:2));
+            obj.dims = str2double(parameter_cell{3});
+        end
+
+        function Tasks = getTasks(obj)
+            Tasks(1) = benchmark_CEC10_CSO(16, obj.dims);
+        end
+    end
+end
