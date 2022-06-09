e75ffc263f3cd37e7b96f37a1f5c1a447dd15111
intLyc <int_lyc@outlook.com>
2022-06-09 21:51:28 +0800

    add CEC06_CSO

---
+++ Problems/Single-task/CEC06_CSO/Class/CEC06_CSO24.m
@@ -0,0 +1,21 @@
+classdef CEC06_CSO24 < Problem
+    % <Single> <Constrained>
+
+    %------------------------------- Copyright --------------------------------
+    % Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
+    % research purposes. All publications which use this platform or any code
+    % in the platform should acknowledge the use of "MTO-Platform" and cite
+    % or footnote "https://github.com/intLyc/MTO-Platform"
+    %--------------------------------------------------------------------------
+
+    methods
+        function obj = CEC06_CSO24(name)
+            obj = obj@Problem(name);
+            obj.sub_eva = 500000;
+        end
+
+        function Tasks = getTasks(obj)
+            Tasks(1) = benchmark_CEC06_CSO(24);
+        end
+    end
+end
