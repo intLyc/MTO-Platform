eb0b5922b689c8c2d0a8f4e80459f08818bf29a9
int-lyc <int_lyc@outlook.com>
2022-08-21 22:57:37 +0800

    add Constrained Single-objective ATF problems

---
+++ Problems/Single-task/ATF/ATF1.m
@@ -0,0 +1,25 @@
+classdef ATF1 < Problem
+    % <Single> <Constrained>
+
+    %------------------------------- Copyright --------------------------------
+    % Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
+    % research purposes. All publications which use this platform or any code
+    % in the platform should acknowledge the use of "MTO-Platform" and cite
+    % or footnote "https://github.com/intLyc/MTO-Platform"
+    %--------------------------------------------------------------------------
+
+    methods
+        function obj = ATF1(varargin)
+            obj = obj@Problem(varargin);
+            obj.sub_eva = 60 * 500;
+        end
+
+        function Tasks = getTasks(obj)
+            Task.Lb = [-10, -5];
+            Task.Ub = [5, 5];
+            Task.dims = 2;
+            Task.fnc = @(x)ATF_Func(x, 1);
+            Tasks(1) = Task;
+        end
+    end
+end
