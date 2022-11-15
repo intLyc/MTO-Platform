4396ba36589b10fcd95084409e3fa1dad4ba3d97
intLyc <int_lyc@outlook.com>
2022-11-15 13:32:54 +0800

    update

--- MTO/Problems/Multi-objective Multi-task/CEC19-MaTMO/CEC19_MaTMO5.m
+++ MTO/Problems/Multi-objective Multi-task/CEC19-MaTMO/CEC19_MaTMO5.m
@@ -1,5 +1,5 @@
 classdef CEC19_MaTMO5 < Problem
-    % <MaT-MO> <None>
+    % <Many-task> <Multi-objective> <None>
 
     %------------------------------- Copyright --------------------------------
     % Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
