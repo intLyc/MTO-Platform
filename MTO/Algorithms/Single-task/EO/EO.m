b973b18fb356c5abfcc9325d6faf66f64557663c
int-lyc <int_lyc@outlook.com>
2022-12-16 23:06:29 +0800

    add meta-heuriastic-algorithms

---
+++ MTO/Algorithms/Single-task/EO/EO.m
@@ -0,0 +1,109 @@
+classdef EO < Algorithm
+    % <Single-task> <Single-objective> <None/Constrained>
+
+    %------------------------------- Reference --------------------------------
+    % @Article{Faramarzi2020EO,
+    %   title   = {Equilibrium Optimizer: A Novel Optimization Algorithm},
+    %   author  = {Afshin Faramarzi and Mohammad Heidarinejad and Brent Stephens and Seyedali Mirjalili},
+    %   journal = {Knowledge-Based Systems},
+    %   year    = {2020},
+    %   issn    = {0950-7051},
+    %   pages   = {105190},
+    %   volume  = {191},
+    % }
+    %--------------------------------------------------------------------------
+
+    %------------------------------- Copyright --------------------------------
+    % Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
+    % research purposes. All publications which use this platform or any code
+    % in the platform should acknowledge the use of "MTO-Platform" and cite
+    % or footnote "https://github.com/intLyc/MTO-Platform"
+    %--------------------------------------------------------------------------
+
+    properties (SetAccess = private)
+        A1 = 2
+        A2 = 1
+        V = 1
+        GP = 0.5
+    end
+
+    methods
+        function Parameter = getParameter(Algo)
+            Parameter = {'A1', num2str(Algo.A1), ...
+                             'A2', num2str(Algo.A2), ...
+                             'V', num2str(Algo.V), ...
+                             'GP', num2str(Algo.GP)};
+        end
+
+        function Algo = setParameter(Algo, Parameter)
+            i = 1;
+            Algo.A1 = str2double(Parameter{i}); i = i + 1;
+            Algo.A2 = str2double(Parameter{i}); i = i + 1;
+            Algo.V = str2double(Parameter{i}); i = i + 1;
+            Algo.GP = str2double(Parameter{i}); i = i + 1;
+        end
+
+        function run(Algo, Prob)
+            % Initialization
+            population = Initialization(Algo, Prob, Individual);
+            dim = max(Prob.D);
+            for t = 1:Prob.T
+                for i = 1:4
+                    Ceq{t}(i) = Individual();
+                    Ceq{t}(i).Dec = zeros(1, dim);
+                    Ceq{t}(i).Obj = inf;
+                    Ceq{t}(i).CV = inf;
+                end
+            end
+
+            while Algo.notTerminated(Prob)
+                for t = 1:Prob.T
+                    for i = 1:Prob.N
+                        pObj = population{t}(i).Obj;
+                        pCV = population{t}(i).CV;
+                        cObjs = Ceq{t}.Objs;
+                        cCVs = Ceq{t}.CVs;
+                        if pCV < cCVs(1) || (pCV == cCVs(1) && pObj < cObjs(1))
+                            Ceq{t}(1) = population{t}(i);
+                        elseif pCV < cCVs(2) || (pCV == cCVs(2) && pObj < cObjs(2))
+                            Ceq{t}(2) = population{t}(i);
+                        elseif pCV < cCVs(3) || (pCV == cCVs(3) && pObj < cObjs(3))
+                            Ceq{t}(3) = population{t}(i);
+                        elseif pCV < cCVs(4) || (pCV == cCVs(4) && pObj < cObjs(4))
+                            Ceq{t}(4) = population{t}(i);
+                        end
+                    end
+                    Ceq_ave = Individual();
+                    Ceq_ave.Dec = mean(Ceq{t}.Decs);
+                    C_pool = [Ceq{t}, Ceq_ave];
+                    ratio = (1 - Algo.FE / Prob.maxFE) ^ (Algo.A2 * Algo.FE / Prob.maxFE);
+                    % Generation
+                    offspring = Algo.Generation(population{t}, C_pool, ratio);
+                    % Evaluation
+                    offspring = Algo.Evaluation(offspring, Prob, t);
+                    % Selection
+                    population{t} = Selection_Tournament(population{t}, offspring);
+                end
+            end
+        end
+
+        function offspring = Generation(Algo, population, C_pool, ratio)
+            dim = length(population(1).Dec);
+            for i = 1:length(population)
+                offspring(i) = population(i);
+                lambda = rand(1, dim);
+                r = rand(1, dim);
+                CeqDec = C_pool(randi(length(C_pool))).Dec;
+                F = Algo.A1 * sign(r - 0.5) .* (exp(-lambda .* ratio) - 1);
+                r1 = rand(); r2 = rand();
+                GCP = 0.5 * r1 * ones(1, dim) * (r2 >= Algo.GP);
+                G0 = GCP .* (CeqDec - lambda .* population(i).Dec);
+                G = G0 .* F;
+                offspring(i).Dec = CeqDec + (population(i).Dec - CeqDec) .* F + (G ./ lambda * Algo.V) .* (1 - F);
+
+                offspring(i).Dec(offspring(i).Dec > 1) = 1;
+                offspring(i).Dec(offspring(i).Dec < 0) = 0;
+            end
+        end
+    end
+end
---
+++ MTO/Algorithms/Single-task/MPA/MPA.m
@@ -0,0 +1,123 @@
+classdef MPA < Algorithm
+    % <Single-task> <Single-objective> <None/Constrained>
+
+    %------------------------------- Reference --------------------------------
+    % @Article{Faramarzi2020MPA,
+    %   title   = {Marine Predators Algorithm: A Nature-inspired Metaheuristic},
+    %   author  = {Afshin Faramarzi and Mohammad Heidarinejad and Seyedali Mirjalili and Amir H. Gandomi},
+    %   journal = {Expert Systems with Applications},
+    %   year    = {2020},
+    %   issn    = {0957-4174},
+    %   pages   = {113377},
+    %   volume  = {152},
+    % }
+    %--------------------------------------------------------------------------
+
+    %------------------------------- Copyright --------------------------------
+    % Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
+    % research purposes. All publications which use this platform or any code
+    % in the platform should acknowledge the use of "MTO-Platform" and cite
+    % or footnote "https://github.com/intLyc/MTO-Platform"
+    %--------------------------------------------------------------------------
+
+    properties (SetAccess = private)
+        FADs = 0.2
+        P = 0.5
+    end
+
+    methods
+        function Parameter = getParameter(Algo)
+            Parameter = {'FADs', num2str(Algo.FADs), ...
+                             'P', num2str(Algo.P)};
+        end
+
+        function Algo = setParameter(Algo, Parameter)
+            i = 1;
+            Algo.FADs = str2double(Parameter{i}); i = i + 1;
+            Algo.P = str2double(Parameter{i}); i = i + 1;
+        end
+
+        function run(Algo, Prob)
+            % Initialization
+            population = Initialization(Algo, Prob, Individual);
+            dim = max(Prob.D);
+
+            while Algo.notTerminated(Prob)
+                for t = 1:Prob.T
+                    CF = (1 - Algo.FE / Prob.maxFE) ^ (2 * Algo.FE / Prob.maxFE);
+                    RL = 0.05 * Algo.Levy(Prob.N, dim, 1.5);
+                    RB = randn(Prob.N, dim);
+                    if Algo.FE < Prob.maxFE / 3
+                        Phase = 1;
+                    elseif Algo.FE < Prob.maxFE * 2/3
+                        Phase = 2;
+                    else
+                        Phase = 3;
+                    end
+                    % Generation
+                    offspring = Algo.Generation(population{t}, t, CF, RL, RB, Phase);
+
+                    % Evaluation
+                    offspring = Algo.Evaluation(offspring, Prob, t);
+                    % Selection
+                    population{t} = Selection_Tournament(population{t}, offspring);
+                end
+            end
+        end
+
+        function offspring = Generation(Algo, population, t, CF, RL, RB, Phase)
+            n = length(population);
+            dim = length(population(1).Dec);
+            for i = 1:n
+                offspring(i) = population(i);
+                R = rand(1, dim);
+                switch Phase
+                    case 1
+                        stepsize = RB(i, :) .* (Algo.Best{t}.Dec - RB(i, :) .* population(i).Dec);
+                        offspring(i).Dec = population(i).Dec + Algo.P * R .* stepsize;
+                    case 2
+                        if i > n / 2
+                            stepsize = RB(i, :) .* (RB(i, :) .* Algo.Best{t}.Dec - population(i).Dec);
+                            offspring(i).Dec = Algo.Best{t}.Dec + Algo.P * CF * stepsize;
+                        else
+                            stepsize = RL(i, :) .* (Algo.Best{t}.Dec - RL(i, :) .* population(i).Dec);
+                            offspring(i).Dec = population(i).Dec + Algo.P * R .* stepsize;
+                        end
+                    case 3
+                        stepsize = RL(i, :) .* (RL(i, :) .* Algo.Best{t}.Dec - population(i).Dec);
+                        offspring(i).Dec = Algo.Best{t}.Dec + Algo.P * CF * stepsize;
+                end
+
+            end
+
+            if rand() < Algo.FADs
+                U = rand(n, dim) < Algo.FADs;
+                for i = 1:n
+                    offspring(i).Dec = offspring(i).Dec + CF * rand(1, dim) .* U(i, :);
+                end
+            else
+                r = rand();
+                ridx1 = randperm(n);
+                ridx2 = randperm(n);
+                for i = 1:n
+                    stepsize = (Algo.FADs * (1 - r) + r) * (offspring(ridx1(i)).Dec - offspring(ridx2(i)).Dec);
+                    offspring(i).Dec = offspring(i).Dec + stepsize;
+                end
+            end
+
+            for i = 1:n
+                offspring(i).Dec(offspring(i).Dec > 1) = 1;
+                offspring(i).Dec(offspring(i).Dec < 0) = 0;
+            end
+        end
+
+        function [z] = Levy(Algo, n, m, beta)
+            num = gamma(1 + beta) * sin(pi * beta / 2); % used for Numerator
+            den = gamma((1 + beta) / 2) * beta * 2 ^ ((beta - 1) / 2); % used for Denominator
+            sigma_u = (num / den) ^ (1 / beta); % Standard deviation
+            u = random('Normal', 0, sigma_u, n, m);
+            v = random('Normal', 0, 1, n, m);
+            z = u ./ (abs(v) .^ (1 / beta));
+        end
+    end
+end
