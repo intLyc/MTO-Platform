1f7b906b856e25d3fba0c18e6fbe73646235dde6
intLyc <int_lyc@outlook.com>
2023-04-11 16:07:30 +0800

    add CEC22-SO

---
+++ MTO/Problems/Single-task/CEC22-SO/CEC22_SO4.m
@@ -0,0 +1,60 @@
+classdef CEC22_SO4 < Problem
+% <Single-task> <Single-objective> <None>
+
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "MTO-Platform" and cite
+% or footnote "https://github.com/intLyc/MTO-Platform"
+%--------------------------------------------------------------------------
+
+methods
+    function Prob = CEC22_SO4(varargin)
+        Prob = Prob@Problem(varargin);
+        Prob.maxFE = 20000 * Prob.D;
+    end
+
+    function Parameter = getParameter(Prob)
+        Parameter = {'Dim', num2str(Prob.D)};
+        Parameter = [Prob.getRunParameter(), Parameter];
+    end
+
+    function Prob = setParameter(Prob, Parameter)
+        D = str2double(Parameter{3});
+        if D > 20
+            D = 20;
+        elseif D > 10 && D < 20
+            D = 10;
+        elseif D < 2
+            D = 2;
+        end
+        if Prob.D == D
+            Prob.setRunParameter(Parameter(1:2));
+        else
+            Prob.D = D;
+            Prob.maxFE = 20000 * Prob.D;
+            Prob.setRunParameter({Parameter{1}, num2str(Prob.maxFE)});
+        end
+    end
+
+    function setTasks(Prob)
+        if isempty(Prob.D)
+            D = Prob.defaultD;
+            if D > 20
+                D = 20;
+            elseif D > 10 && D < 20
+                D = 10;
+            elseif D < 2
+                D = 2;
+            end
+            Prob.D = D;
+        end
+        Tasks(1) = benchmark_CEC22_SO(4, Prob.D);
+        Prob.T = 1;
+        Prob.D(1) = Tasks(1).Dim;
+        Prob.Fnc{1} = Tasks(1).Fnc;
+        Prob.Lb{1} = Tasks(1).Lb;
+        Prob.Ub{1} = Tasks(1).Ub;
+    end
+end
+end
---
+++ MTO/Problems/Single-task/CEC22-SO/benchmark_CEC22_SO.m
@@ -0,0 +1,32 @@
+function Tasks = benchmark_CEC22_SO(index, dim)
+
+%------------------------------- Reference --------------------------------
+% @Article{Kumar2022CEC22-SO,
+%   title      = {Problem Definitions and Evaluation Criteria for the CEC 2022 Special Session and Competition on Single Objective Bound Constrained Numerical Optimization},
+%   author     = {Abhishek Kumar and Kenneth V. Price and Ali Wagdy Mohamed and Anas A. Hadi and P. N.Suganthan},
+%   year       = {2022},
+% }
+%--------------------------------------------------------------------------
+
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "MTO-Platform" and cite
+% or footnote "https://github.com/intLyc/MTO-Platform"
+%--------------------------------------------------------------------------
+% Global optima
+g = [300, ... % Unimodal Function 1
+        400, 600, 800, 900, ... % Basic Functions 2,3,4,5
+        1800, 2000, 2200, ... % Hybrid Functions 6,7,8
+        2300, 2400, 2600, 2700]; % Composition Functions 9,10,11,12
+
+Tasks.Dim = dim;
+Tasks.Lb = -100 * ones(1, dim);
+Tasks.Ub = 100 * ones(1, dim);
+Tasks.Fnc = @(x)get_func(x, index, g(index));
+end
+
+function [Obj, Con] = get_func(x, index, g)
+Obj = cec22_test_func(x', index) - g;
+Con = 0;
+end
