5e9b9f9ee68dbdf5f848537c536e8b59e293d3d0
intLyc <int_lyc@outlook.com>
2023-04-11 16:19:26 +0800

    WCCI20-MTSO Error Value Type

---
+++ MTO/Problems/Multi-task/WCCI20-MTSO/ErrorValueType/WCCI20_MTSO_EV8.m
@@ -0,0 +1,28 @@
+classdef WCCI20_MTSO_EV8 < Problem
+% <Multi-task> <Single-objective> <None>
+
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "MTO-Platform" and cite
+% or footnote "https://github.com/intLyc/MTO-Platform"
+%--------------------------------------------------------------------------
+
+methods
+    function Prob = WCCI20_MTSO_EV8(varargin)
+        Prob = Prob@Problem(varargin);
+        Prob.maxFE = 1000 * 200;
+    end
+
+    function setTasks(Prob)
+        Tasks = benchmark_WCCI20_MTSO_EV(8);
+        Prob.T = length(Tasks);
+        for t = 1:Prob.T
+            Prob.D(t) = Tasks(t).Dim;
+            Prob.Fnc{t} = Tasks(t).Fnc;
+            Prob.Lb{t} = Tasks(t).Lb;
+            Prob.Ub{t} = Tasks(t).Ub;
+        end
+    end
+end
+end
