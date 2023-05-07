87cdac900db66b8ad3df8f6c2ad6ea39a4f25478
intLyc <int_lyc@outlook.com>
2023-05-07 13:37:06 +0800

    draw utils

---
+++ MTO/Algorithms/Utils/DrawUtils/DrawPop.m
@@ -0,0 +1,50 @@
+classdef DrawPop < handle
+
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "MTO-Platform" and cite
+% or footnote "https://github.com/intLyc/MTO-Platform"
+%--------------------------------------------------------------------------
+
+properties (SetAccess = private)
+    tiled
+    fig
+    ax_list
+end
+
+methods
+    function obj = init(obj, Algo, Prob)
+        obj.fig = figure();
+        obj.tiled = tiledlayout('flow');
+        obj.tiled.TileSpacing = 'compact';
+        obj.tiled.Padding = 'compact';
+        title(obj.tiled, [Algo.Name, ' on ', Prob.Name]);
+        xlabel(obj.tiled, 'Dimension');
+        ylabel(obj.tiled, 'Unified Decision Variables');
+        for t = 1:Prob.T
+            nexttile
+        end
+    end
+
+    function obj = update(obj, Prob, Pop)
+        for t = 1:Prob.T
+            cla(obj.tiled.Children(end - t + 1))
+            for i = 1:length(Pop{t})
+                p = plot(obj.tiled.Children(end - t + 1), Pop{t}(i).Dec);
+                p.Color = [0.2, 0.2, 0.2];
+                p.LineWidth = 1;
+                hold(obj.tiled.Children(end - t + 1), 'on');
+            end
+            xlim(obj.tiled.Children(end - t + 1), [1, max(Prob.D)]);
+            ylim(obj.tiled.Children(end - t + 1), [0, 1]);
+            title(obj.tiled.Children(end - t + 1), ['Task ', num2str(t)]);
+            drawnow;
+        end
+    end
+
+    function obj = close(obj)
+        close(obj.fig)
+    end
+end
+end
