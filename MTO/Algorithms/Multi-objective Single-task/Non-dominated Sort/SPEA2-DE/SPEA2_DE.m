a5fe653c691af2ef1694cc8380574c5b25151ee7
intLyc <int_lyc@outlook.com>
2023-05-10 19:31:59 +0800

    add

---
+++ MTO/Algorithms/Multi-objective Single-task/Non-dominated Sort/SPEA2-DE/SPEA2_DE.m
@@ -0,0 +1,81 @@
+classdef SPEA2_DE < Algorithm
+% <Single-task> <Multi-objective> <None/Constrained>
+
+%------------------------------- Reference --------------------------------
+% @InProceedings{Zitzler2001SPEA2,
+%   title     = {SPEA2: Improving the Strength Pareto Evolutionary Algorithm For Multiobjective Optimization},
+%   author    = {Zitzler, E. and Laumanns, M. and Thiele, L.},
+%   booktitle = {Evolutionary Methods for Design, Optimization and Control with Applications to Industrial Problems. Proceedings of the EUROGEN'2001. Athens. Greece, September 19-21},
+%   year      = {2001},
+% }
+%--------------------------------------------------------------------------
+
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "MTO-Platform" and cite
+% or footnote "https://github.com/intLyc/MTO-Platform"
+%--------------------------------------------------------------------------
+
+% The code implementation is referenced from PlatEMO(https://github.com/BIMK/PlatEMO).
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2022 BIMK Group. You are free to use the PlatEMO for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
+% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
+% for evolutionary multi-objective optimization [educational forum], IEEE
+% Computational Intelligence Magazine, 2017, 12(4): 73-87".
+%--------------------------------------------------------------------------
+
+properties (SetAccess = private)
+    F = 0.5
+    CR = 0.9
+end
+
+methods
+    function Parameter = getParameter(Algo)
+        Parameter = {'F: Mutation Factor', num2str(Algo.F), ...
+                'CR: Crossover Rate', num2str(Algo.CR)};
+    end
+
+    function setParameter(Algo, Parameter)
+        i = 1;
+        Algo.F = str2double(Parameter{i}); i = i + 1;
+        Algo.CR = str2double(Parameter{i}); i = i + 1;
+    end
+
+    function run(Algo, Prob)
+        % Initialize
+        population = Initialization(Algo, Prob, Individual);
+        for t = 1:Prob.T
+            [population{t}, Fitness{t}] = Selection_SPEA2(population{t}, Prob.N);
+        end
+
+        while Algo.notTerminated(Prob, population)
+            for t = 1:Prob.T
+                % Generation
+                offspring = Algo.Generation(population{t});
+                % Evaluation
+                offspring = Algo.Evaluation(offspring, Prob, t);
+                % Selection
+                population{t} = [population{t}, offspring];
+                [population{t}, Fitness{t}] = Selection_SPEA2(population{t}, Prob.N);
+            end
+        end
+    end
+
+    function offspring = Generation(Algo, population)
+        for i = 1:length(population)
+            offspring(i) = population(i);
+            A = randperm(length(population), 4);
+            A(A == i) = []; x1 = A(1); x2 = A(2); x3 = A(3);
+
+            offspring(i).Dec = population(x1).Dec + Algo.F * (population(x2).Dec - population(x3).Dec);
+            offspring(i).Dec = DE_Crossover(offspring(i).Dec, population(i).Dec, Algo.CR);
+
+            offspring(i).Dec(offspring(i).Dec > 1) = 1;
+            offspring(i).Dec(offspring(i).Dec < 0) = 0;
+        end
+    end
+end
+end
