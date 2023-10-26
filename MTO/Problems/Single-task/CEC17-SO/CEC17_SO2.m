0d6e1b334e94a513b64c9594b96aad10f2fceabf
intLyc <int_lyc@outlook.com>
2023-10-26 19:40:38 +0800

    Add CEC17-SO

---
+++ MTO/Problems/Single-task/CEC17-SO/CEC17_SO2.m
@@ -0,0 +1,45 @@
+classdef CEC17_SO2 < Problem
+% <Single-task> <Single-objective> <None>
+
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "MTO-Platform" and cite
+% or footnote "https://github.com/intLyc/MTO-Platform"
+%--------------------------------------------------------------------------
+
+methods
+    function Prob = CEC17_SO2(varargin)
+        Prob = Prob@Problem(varargin);
+        Prob.maxFE = 10000 * Prob.D;
+    end
+
+    function Parameter = getParameter(Prob)
+        Parameter = {'Dim', num2str(Prob.D)};
+        Parameter = [Prob.getRunParameter(), Parameter];
+    end
+
+    function Prob = setParameter(Prob, Parameter)
+        D = str2double(Parameter{3});
+        if Prob.D == D
+            Prob.setRunParameter(Parameter(1:2));
+        else
+            Prob.D = D;
+            Prob.maxFE = 10000 * Prob.D;
+            Prob.setRunParameter({Parameter{1}, num2str(Prob.maxFE)});
+        end
+    end
+
+    function setTasks(Prob)
+        if isempty(Prob.D)
+            Prob.D = Prob.defaultD;
+        end
+        Tasks(1) = benchmark_CEC17_SO(2, Prob.D);
+        Prob.T = 1;
+        Prob.D(1) = Tasks(1).Dim;
+        Prob.Fnc{1} = Tasks(1).Fnc;
+        Prob.Lb{1} = Tasks(1).Lb;
+        Prob.Ub{1} = Tasks(1).Ub;
+    end
+end
+end
---
+++ MTO/Problems/Single-task/CEC17-SO/benchmark_CEC17_SO.m
@@ -0,0 +1,33 @@
+function Tasks = benchmark_CEC17_SO(index, dim)
+
+%------------------------------- Reference --------------------------------
+% @Article{Awad2017CEC17-SO,
+%   author     = {N. H. Awad and M. Z. Ali and J. J. Liang and B. Y. Qu and P. N. Suganthan},
+%   journal    = {Technical Report, Nanyang Technological University, Singapore},
+%   title      = {Problem Definitions and Evaluation Criteria for the CEC 2017 Special Session and Competition on Single Objective Bound Constrained Real-Parameter Numerical Optimization},
+%   year       = {2017},
+% }
+%--------------------------------------------------------------------------
+
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "MTO-Platform" and cite
+% or footnote "https://github.com/intLyc/MTO-Platform"
+%--------------------------------------------------------------------------
+
+g = (1:30) * 100;
+if index >= 2
+    index = index + 1; % F2 has been deleted
+end
+
+Tasks.Dim = dim;
+Tasks.Lb = -100 * ones(1, dim);
+Tasks.Ub = 100 * ones(1, dim);
+Tasks.Fnc = @(x)get_func(x, index, g(index));
+end
+
+function [Obj, Con] = get_func(x, index, g)
+Obj = cec17_func(x', index) - g;
+Con = 0;
+end
