0b065a15ac3b35ac2390a0a0a780fd64c65d3fce
intLyc <int_lyc@outlook.com>
2024-03-18 19:38:13 +0800

    Add Draw Utils

---
+++ MTO/Algorithms/Utils/DrawUtils/DrawAllPopObj.m
@@ -0,0 +1,126 @@
+classdef DrawAllPopObj < handle
+
+%------------------------------- Copyright --------------------------------
+% Copyright (c) Yanchi Li. You are free to use the MToP for research
+% purposes. All publications which use this platform should acknowledge
+% the use of "MToP" or "MTO-Platform" and cite as "Y. Li, W. Gong, F. Ming,
+% T. Zhang, S. Li, and Q. Gu, MToP: A MATLAB Optimization Platform for
+% Evolutionary Multitasking, 2023, arXiv:2312.08134"
+%--------------------------------------------------------------------------
+
+properties (SetAccess = private)
+    fig
+    ax
+    hplot
+end
+
+methods
+    function obj = DrawAllPopObj(Algo, Prob)
+        obj.fig = figure();
+        obj.ax = axes();
+        title(obj.ax, [Algo.Name, ' on ', Prob.Name]);
+
+        optimum = Prob.getOptimum();
+
+        M = max(Prob.M);
+        if M == 2
+            xlabel(obj.ax, '$f_1$', 'interpreter', 'latex');
+            ylabel(obj.ax, '$f_2$', 'interpreter', 'latex');
+            grid(obj.ax, 'on');
+        elseif M == 3
+            xlabel(obj.ax, '$f_1$', 'interpreter', 'latex');
+            ylabel(obj.ax, '$f_2$', 'interpreter', 'latex');
+            zlabel(obj.ax, '$f_3$', 'interpreter', 'latex');
+            view(obj.ax, [135 30]);
+            grid(obj.ax, 'on');
+        else
+            xlabel(obj.ax, 'Dimension', 'interpreter', 'latex');
+            ylabel(obj.ax, '$f$', 'interpreter', 'latex');
+        end
+
+        color = colororder;
+        for t = 1:Prob.T
+            if M == 2
+                % draw optimum
+                s = scatter(obj.ax, optimum{t}(:, 1), optimum{t}(:, 2));
+                s.MarkerEdgeColor = 'none';
+                s.MarkerFaceAlpha = 0.65;
+                s.MarkerFaceColor = [.2, .2, .2];
+                s.SizeData = 3;
+                hold(obj.ax, 'on');
+
+                % draw objs
+                obj.hplot{t} = scatter(obj.ax, 0, 0);
+                obj.hplot{t}.MarkerEdgeColor = color(mod(t - 1, size(color, 1)) + 1, :);
+                obj.hplot{t}.MarkerFaceAlpha = 0.65;
+                obj.hplot{t}.MarkerFaceColor = color(mod(t - 1, size(color, 1)) + 1, :);
+                obj.hplot{t}.SizeData = 40;
+                hold(obj.ax, 'on');
+            elseif M == 3
+                % draw optimum
+                s = scatter3(obj.ax, optimum{t}(:, 1), optimum{t}(:, 2), optimum{t}(:, 3));
+                s.MarkerEdgeColor = 'none';
+                s.MarkerFaceAlpha = 0.65;
+                s.MarkerFaceColor = [.5, .5, .5];
+                s.SizeData = 3;
+                hold(obj.ax, 'on');
+
+                % draw objs
+                obj.hplot{t} = scatter3(obj.ax, 0, 0, 0);
+                obj.hplot{t}.MarkerEdgeColor = color(mod(t - 1, size(color, 1)) + 1, :);
+                obj.hplot{t}.MarkerFaceAlpha = 0.65;
+                obj.hplot{t}.MarkerFaceColor = color(mod(t - 1, size(color, 1)) + 1, :);
+                obj.hplot{t}.SizeData = 40;
+                hold(obj.ax, 'on');
+            else
+                obj.hplot{t} = {};
+                for i = 1:Prob.N
+                    obj.hplot{t}{i} = plot(obj.ax, 0);
+                    obj.hplot{t}{i}.Color = [0, 0.4470, 0.7410];
+                    obj.hplot{t}{i}.LineWidth = 1;
+                    hold(obj.ax, 'on');
+                end
+            end
+            drawnow;
+        end
+    end
+
+    function obj = update(obj, Algo, Prob, Pop)
+        for t = 1:Prob.T
+            M = Prob.M(t);
+            Obj = Pop{t}.Objs;
+            if M == 2
+                set(obj.hplot{t}, 'XData', Obj(:, 1), 'YData', Obj(:, 2));
+            elseif M == 3
+                % draw optimum
+                set(obj.hplot{t}, 'XData', Obj(:, 1), 'YData', Obj(:, 2), 'ZData', Obj(:, 3));
+            else
+                for i = 1:Prob.N
+                    set(obj.hplot{t}{i}, 'YData', Obj(i, :));
+                end
+            end
+            drawnow;
+        end
+        title(obj.ax, [Algo.Name, ' on ', Prob.Name, ' Gen=', num2str(Algo.Gen)]);
+        M = max(Prob.M);
+        if M == 2
+            xlabel(obj.ax, '$f_1$', 'interpreter', 'latex');
+            ylabel(obj.ax, '$f_2$', 'interpreter', 'latex');
+            grid(obj.ax, 'on');
+        elseif M == 3
+            xlabel(obj.ax, '$f_1$', 'interpreter', 'latex');
+            ylabel(obj.ax, '$f_2$', 'interpreter', 'latex');
+            zlabel(obj.ax, '$f_3$', 'interpreter', 'latex');
+            view(obj.ax, [135 30]);
+            grid(obj.ax, 'on');
+        else
+            xlabel(obj.ax, 'Dimension', 'interpreter', 'latex');
+            ylabel(obj.ax, '$f$', 'interpreter', 'latex');
+        end
+    end
+
+    function obj = close(obj)
+        close(obj.fig)
+    end
+end
+end
