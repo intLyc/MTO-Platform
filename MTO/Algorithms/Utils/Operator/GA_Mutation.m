add8ff5a8427ae3fde25309b66cb57537c9e1c63
intLyc <int_lyc@outlook.com>
2024-07-20 15:26:38 +0800

    GA mutation probability para

--- MTO/Algorithms/Utils/Operator/GA_Mutation.m
+++ MTO/Algorithms/Utils/Operator/GA_Mutation.m
@@ -1,9 +1,16 @@
-function Dec = GA_Mutation(Dec, mu)
+function Dec = GA_Mutation(Dec, mu, varargin)
 % Polynomial mutation
 
 D = size(Dec, 2);
+
+if nargin == 2
+    prob_m = 1 / D;
+else
+    prob_m = varargin{1};
+end
+
 for d = 1:D
-    if rand() < 1 / D
+    if rand() < prob_m
         u = rand(1);
         if u <= 0.5
             delta = ((2 * u + (1 - 2 * u) * (1 - Dec(d))^(mu + 1)))^(1 / (mu + 1)) - 1;
