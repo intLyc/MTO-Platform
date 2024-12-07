6d462fc1f612f9be7677dfd7a3c68b421c439e58
intLyc <int_lyc@outlook.com>
2024-12-07 20:26:35 +0800

    Fix nan Bug

--- MTO/Metrics/Utils/getIGDp.m
+++ MTO/Metrics/Utils/getIGDp.m
@@ -10,7 +10,7 @@ function score = getIGDp(PopObj, optimum)
 % Computational Intelligence Magazine, 2017, 12(4): 73-87".
 %--------------------------------------------------------------------------
 
-if size(PopObj, 2) ~= size(optimum, 2)
+if size(PopObj,1) == 0 || size(PopObj, 2) ~= size(optimum, 2)
     score = nan;
 else
     [Nr, M] = size(optimum);
