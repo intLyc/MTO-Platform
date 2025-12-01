45c9d3c9dfa160f35dc007289596fd16e93d8411
intLyc <int_lyc@cug.edu.cn>
2025-12-01 16:08:43 +0800

    Refactor penalty calculation with obj scale

--- MTO/Algorithms/Utils/BoundaryHandling/RankWithBoundaryHandling.m
+++ MTO/Algorithms/Utils/BoundaryHandling/RankWithBoundaryHandling.m
@@ -2,10 +2,14 @@ function rank = RankWithBoundaryHandling(sample, Prob)
 % Rank the population with boundary constraint handling
 if Prob.Bounded
     % Penalty method
+    currentObjs = sample.Objs;
+    objScale = max(abs(currentObjs));
+    if objScale < 1, objScale = 1; end
     penalty = zeros(length(sample), 1);
     for i = 1:length(sample)
         tempDec = max(0, min(1, sample(i).Dec));
-        penalty(i) = sum((sample(i).Dec - tempDec).^2);
+        violation = sum((sample(i).Dec - tempDec).^2);
+        penalty(i) = violation * objScale;
     end
     % get rank based on constraint and objective
     [~, rank] = sortrows([sample.CVs, sample.Objs + penalty], [1, 2]);
