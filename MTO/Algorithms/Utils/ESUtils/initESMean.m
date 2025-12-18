5723307a0b111aa847a50c061958f074519706a2
intLyc <int_lyc@cug.edu.cn>
2025-12-18 20:11:31 +0800

    Fix normalization of initial solution mean in initESMean function

--- MTO/Algorithms/Utils/ESUtils/initESMean.m
+++ MTO/Algorithms/Utils/ESUtils/initESMean.m
@@ -2,6 +2,7 @@ function x = initESMean(Prob, t)
 % Initialize the mean solution for task t
 if isprop(Prob, 'initSolutionMean') && ~isempty(Prob.initSolutionMean{t})
     x = Prob.initSolutionMean{t};
+    x = (x - Prob.Lb{t}) ./ (Prob.Ub{t} - Prob.Lb{t});
 else
     x = rand(1, Prob.D(t));
 end
