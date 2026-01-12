cfde2db9d57a4bad373a9a6d4c2139a2211f64ec
intLyc <int_lyc@cug.edu.cn>
2026-01-12 19:17:45 +0800

    Update sampling method

--- MTO/Algorithms/Single-task/Evolution Strategy/OpenAI-ES/OpenAI_ES.m
+++ MTO/Algorithms/Single-task/Evolution Strategy/OpenAI-ES/OpenAI_ES.m
@@ -19,7 +19,6 @@ properties (SetAccess = public)
     lr_decay = 0.1 % Decay rate for learning rate
     beta1 = 0.9 % Exponential decay rate for the first moment estimates
     beta2 = 0.999 % Exponential decay rate for the second moment estimates
-    epsilon = 1e-8 % Small constant to prevent division by zero
 end
 
 methods
@@ -29,8 +28,7 @@ methods
                 'learning rate', num2str(Algo.lr), ...
                 'lr decay', num2str(Algo.lr_decay), ...
                 'beta1', num2str(Algo.beta1), ...
-                'beta2', num2str(Algo.beta2), ...
-                'epsilon', num2str(Algo.epsilon)};
+                'beta2', num2str(Algo.beta2)};
     end
 
     function Algo = setParameter(Algo, Parameter)
@@ -40,7 +38,6 @@ methods
         Algo.lr_decay = str2double(Parameter{4});
         Algo.beta1 = str2double(Parameter{5});
         Algo.beta2 = str2double(Parameter{6});
-        Algo.epsilon = str2double(Parameter{7});
     end
 
     function run(Algo, Prob)
@@ -73,9 +70,8 @@ methods
             current_lr = Algo.lr * lr_decay_factor;
 
             for t = 1:Prob.T
-                % ---- Antithetic Sampling ----
-                Z_half = randn(Prob.D(t), N / 2);
-                Z = [Z_half, -Z_half];
+                % ---- Sampling ----
+                Z = randn(Prob.D(t), N);
 
                 % Add noise in normalized space
                 X = repmat(x{t}, 1, N) + current_sigma * Z;
@@ -114,8 +110,8 @@ methods
                 v_hat = v{t} / (1 - Algo.beta2^Algo.Gen);
 
                 % 5. Update parameters
-                % Using '+' because 'grad' points to higher fitness rank (better solution)
-                x{t} = x{t} + current_lr * m_hat ./ (sqrt(v_hat) + Algo.epsilon);
+                % Using '+' because 'grad' points to higher fitness rank
+                x{t} = x{t} + current_lr * m_hat ./ (sqrt(v_hat) +1e-8);
                 if Prob.Bounded
                     x{t} = max(0, min(1, x{t}));
                 end
